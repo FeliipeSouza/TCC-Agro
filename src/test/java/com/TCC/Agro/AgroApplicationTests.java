b1fe3fc038c7754fd1cc40f63547209e134cccfe
Felipe Simoes <lipesimoes25@hotmail.com>
2024-03-13 17:54:25 -0300

    com inicial

---
+++ src/test/java/com/TCC/Agro/AgroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TCC.Agro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
